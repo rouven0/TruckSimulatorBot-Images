a63815181451025fd019e069f258f2db4ec4becb
Rouven Seifert <rouven@rfive.de>
2023-05-02 00:34:00 +0200

    nix: add hydra jobs

--- flake.nix
+++ flake.nix
@@ -4,13 +4,16 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       packages = forAllSystems (system: {
         default = pkgs.${system}.python310Packages.callPackage ./default.nix { };
+      });
+      hydraJobs = forAllSystems (system: {
+        default = self.packages.${system}.default;
       });
       nixosModules.default = import ./module.nix;
 
